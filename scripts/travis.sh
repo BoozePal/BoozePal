35aa216d811a99a5b52d3710989820d19be52119
Nándor Holozsnyák <holi60@gmail.com>
2016-10-02 12:46:50 +0200

    travis file fix - 2

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-set -ev
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+set -e
+branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p');
+echo "Actual branch is:" $branch;
 if [ $branch = "master" ]; 
 then
 	(cd boozepal && mvn clean install) && (cd boozepal/boozepal-ear && mvn -PCloudDeploy wildfly:undeploy && mvn -PCloudDeploy wildfly:deploy);

c214b506f56fbb26ea19c2c7cd93cd65c7e4af3a
Nándor Holozsnyák <holi60@gmail.com>
2016-10-02 20:42:14 +0200

    travis deploy only if the master branch is built

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-set -ev
-git checkout master
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p');
-echo "Actual branch is:" $branch;
-if [ $branch = "master" ]; 
+actualBranch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p');
+commitId=$( echo $actualBranch | awk '{split($0,a," "); print a[4]}');
+cuttedId=$(echo $commitId | cut -c1-7);
+branchMsg=$(git branch --contains $cuttedId);
+neededBranch=$( echo $branchMsg | awk '{split($0,a," "); print a[length(a)]}');
+echo "Actual branch is:" $neededBranch;
+if [ $neededBranch = "master" ]; 
 then
 	(cd boozepal && mvn clean install && cd boozepal-ear && mvn -PCloudDeploy wildfly:undeploy && mvn -PCloudDeploy wildfly:deploy);
 else 

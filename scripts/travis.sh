ff5f938bb63aae6f1a3d4012a1cf82d5a0c10ed2
Nándor Holozsnyák <holi60@gmail.com>
2016-10-02 12:41:37 +0200

    travis deploy fix

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,7 +3,7 @@ set -ev
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 if [ $branch = "master" ]; 
 then
-	(cd boozepal && mvn clean install) && (cd boozepal-ear && mvn -PCloudDeploy wildfly:undeploy && mvn -PCloudDeploy wildfly:deploy);
+	(cd boozepal && mvn clean install) && (cd boozepal/boozepal-ear && mvn -PCloudDeploy wildfly:undeploy && mvn -PCloudDeploy wildfly:deploy);
 else 
 	(cd boozepal && mvn clean install);
 fi

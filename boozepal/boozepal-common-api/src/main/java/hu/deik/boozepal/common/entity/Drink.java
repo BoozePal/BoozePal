94b6b9bb8d2c94729043dd4570c380aa5e0336e2
Nándor Holozsnyák <holi60@gmail.com>
2016-10-10 21:47:39 +0200

    #21: Adatbázis séma kialakítása.
    A WIKI-ben említett entitások létrehozása.

--- boozepal/boozepal-common-api/src/main/java/hu/deik/boozepal/common/entity/Drink.java
+++ boozepal/boozepal-common-api/src/main/java/hu/deik/boozepal/common/entity/Drink.java
@@ -2,31 +2,41 @@ package hu.deik.boozepal.common.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 /**
- * Felhasználóni jogokat reprezentáló entitás.
+ * Italokat reprezentáló entitás.
  * 
  * @version 1.0
+ *
  */
-@Entity
-@Table(name = "boozepal_role")
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "boozepal_drink")
 @Data
 @EqualsAndHashCode(callSuper = false)
-public class Role extends BaseEntity {
+@Builder
+public class Drink extends BaseEntity {
 
     private static final long serialVersionUID = 1L;
+    /**
+     * Ital megnevezése.
+     */
+    @Column(nullable = false, length = 128)
+    private String name;
 
     /**
-     * Jog neve.
+     * Ital típusa.
      */
-    @Column(nullable = false, length = 64)
-    private String roleName;
+    @OneToOne
+    private DrinkType drinkType;
+
 }

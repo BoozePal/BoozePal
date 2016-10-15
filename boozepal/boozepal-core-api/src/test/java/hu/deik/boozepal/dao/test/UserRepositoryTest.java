d84b2ecf65769b7a3595f08f9012d8c2bf3a1a70
Nándor Holozsnyák <holi60@gmail.com>
2016-10-15 11:33:08 +0200

    #30: Felhasználó dao elkészíve.

--- boozepal/boozepal-core-api/src/test/java/hu/deik/boozepal/dao/test/UserRepositoryTest.java
+++ boozepal/boozepal-core-api/src/test/java/hu/deik/boozepal/dao/test/UserRepositoryTest.java
@@ -1,7 +1,7 @@
 package hu.deik.boozepal.dao.test;
 
+import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +9,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import hu.deik.boozepal.common.entity.Coordinate;
 import hu.deik.boozepal.common.entity.User;
 import hu.deik.boozepal.core.repo.UserRepository;
 
@@ -21,6 +22,8 @@ public class UserRepositoryTest {
     private static final String TEST_PASSWORD = "TestPassword";
     private static final String TEST_USER = "TestUser";
 
+    private User user;
+
     @Autowired
     private UserRepository userDao;
 
@@ -31,32 +34,86 @@ public class UserRepositoryTest {
 
     @Test
     public void testSaveNewUser() {
-        User user = createUser();
+        user = createUser();
         User savedUser = userDao.save(user);
         Assert.assertNotNull(savedUser.getId());
-        userDao.delete(savedUser.getId());
     }
 
     @Test
     public void testFindSavedUserByUserName() {
-        User user = createUser();
-        User savedUser = userDao.save(user);
+        user = createUser();
+        userDao.save(user);
         User foundUser = userDao.findByUsername(TEST_USER);
         Assert.assertEquals(TEST_USER, foundUser.getUsername());
-        userDao.delete(savedUser.getId());
     }
 
     @Test
     public void testFindSavedUserByEmail() {
-        User user = createUser();
-        User savedUser = userDao.save(user);
+        user = createUser();
+        userDao.save(user);
         User foundUser = userDao.findByEmail(TEST_EMAIL_COM);
         Assert.assertEquals(TEST_EMAIL_COM, foundUser.getEmail());
-        userDao.delete(savedUser.getId());
     }
 
-    private User createUser() {
-        return User.builder().username(TEST_USER).email(TEST_EMAIL_COM).password(TEST_PASSWORD).remove(false).build();
+    @Test
+    public void testUpdateUserCoordinate() {
+        user = createUser();
+        User savedUser = userDao.save(user);
+        double latitude = 11.0;
+        double altitude = 12.0;
+        userDao.updateUserCoordinate(latitude, altitude, savedUser.getId());
+        User findOne = userDao.findOne(savedUser.getId());
+        Assert.assertEquals(latitude, findOne.getLastKnownCoordinate().getLatitude(), 0.0);
+        Assert.assertEquals(altitude, findOne.getLastKnownCoordinate().getAltitude(), 0.0);
+        // userDao.delete(savedUser.getId());
     }
+
+    @Test
+    public void testUpdatePriceCategory() {
+        user = createUser();
+        User savedUser = userDao.save(user);
+        int priceCategory = 4;
+        userDao.updatePriceCategory(priceCategory, savedUser.getId());
+        User findOne = userDao.findOne(savedUser.getId());
+        Assert.assertEquals(priceCategory, findOne.getPriceCategory(), 0.0);
+        // userDao.delete(savedUser.getId());
+    }
+
+    @Test
+    public void testActivateUser() {
+        user = createUser();
+        User savedUser = userDao.save(user);
+        userDao.activateUser(savedUser.getId());
+        User findOne = userDao.findOne(savedUser.getId());
+        Assert.assertFalse(findOne.isRemove());
+    }
+
+    @Test
+    public void testDeActivateUser() {
+        user = createUser();
+        User savedUser = userDao.save(user);
+        userDao.deactivateUser(savedUser.getId());
+        User findOne = userDao.findOne(savedUser.getId());
+        Assert.assertTrue(findOne.isRemove());
+    }
+
+    @After
+    public void tearDown() {
+        if (user != null && user.getId() != null)
+            userDao.delete(user);
+    }
+
+    //@formatter:off
+    private User createUser() {
+        return User.builder()
+                .username(TEST_USER)
+                .email(TEST_EMAIL_COM)
+                .password(TEST_PASSWORD)
+                .remove(false)
+                .lastKnownCoordinate(new Coordinate(10.0, 10.0))
+                .priceCategory(3)
+                .build();
+    }
+   //@formatter:on
 
 }

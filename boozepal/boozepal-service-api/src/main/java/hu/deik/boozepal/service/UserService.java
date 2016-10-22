e8d9e43313911f75bcad18f432343805f90c46d4
Pusinszky Viktor <luxeon@hotmail.hu>
2016-10-22 23:18:14 +0200

    Table model include total number of members #36

--- boozepal/boozepal-service-api/src/main/java/hu/deik/boozepal/service/UserService.java
+++ boozepal/boozepal-service-api/src/main/java/hu/deik/boozepal/service/UserService.java
@@ -1,5 +1,7 @@
 package hu.deik.boozepal.service;
 
+import java.util.List;
+
 import javax.ejb.Local;
 
 import hu.deik.boozepal.common.entity.User;
@@ -9,4 +11,7 @@ public interface UserService {
 
 	public User findUserByName(String username);
 
+	public List<User> findAll();
+
+	public void save(User user);
 }

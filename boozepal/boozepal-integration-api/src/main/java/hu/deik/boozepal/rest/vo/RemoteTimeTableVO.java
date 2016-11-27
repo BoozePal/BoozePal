cf2494ccc497c48682ee2653a95014ea33fb9e2d
Pusinszky Viktor <luxeon@hotmail.hu>
2016-11-27 15:01:32 +0100

    #45 fix Json property.

--- boozepal/boozepal-integration-api/src/main/java/hu/deik/boozepal/rest/vo/RemoteTimeTableVO.java
+++ boozepal/boozepal-integration-api/src/main/java/hu/deik/boozepal/rest/vo/RemoteTimeTableVO.java
@@ -1,5 +1,7 @@
 package hu.deik.boozepal.rest.vo;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.io.Serializable;
@@ -8,22 +10,20 @@ import java.util.List;
 
 /**
  * Távoli felhasználót repreneztáló érték osztály.
- * 
+ * <p>
  * A távoli felhasználó az akit az Android kliensről "léptetünk" a
  * rendszerünkbe, azaz az adatait letároljuk a mi adatbázisunkban is.
- * 
- * @version 1.0
  *
+ * @version 1.0
  */
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
 @Builder
 @EqualsAndHashCode(callSuper = false)
 public class RemoteTimeTableVO implements Serializable {
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = 1L;
 
@@ -31,8 +31,16 @@ public class RemoteTimeTableVO implements Serializable {
      * Token.
      */
     @NonNull
+    @JsonProperty("token")
     private String token;
 
+    @JsonProperty("timeTableList")
     private List<Date> timeTableList;
 
+    @JsonCreator
+    public RemoteTimeTableVO(@JsonProperty("token") String token, @JsonProperty("timeTableList") List<Date> timeTableList) {
+        super();
+        this.token = token;
+        this.timeTableList = timeTableList;
+    }
 }

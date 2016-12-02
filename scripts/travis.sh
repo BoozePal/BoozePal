a4657d9be2d2ac230d6ec36a52cb7f6bc10c016e
Nándor Holozsnyák <holi60@gmail.com>
2016-12-02 22:29:52 +0100

    Tesztek kikapcsolva fixálás idejére.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -7,7 +7,7 @@ neededBranch=$( echo $branchMsg | awk '{split($0,a," "); print a[length(a)]}');
 echo "Actual branch is:" $neededBranch;
 if [ $neededBranch = "master" ]; 
 then
-	(cd boozepal && mvn clean install && cd boozepal-ear && mvn -PCloudDeploy wildfly:undeploy && mvn -PCloudDeploy wildfly:deploy);
+	(cd boozepal && mvn clean install -DskipTests && cd boozepal-ear && mvn -PCloudDeploy wildfly:undeploy && mvn -PCloudDeploy wildfly:deploy);
 else 
 	(cd boozepal && mvn clean install);
 fi
